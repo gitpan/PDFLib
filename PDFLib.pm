15524677f4413ed69565b87f4475f9f869bfc250
Matt Sergeant <msergeant@cpan.org>
2001-06-03 13:48:31 +0000

    Import of MSERGEANT/PDFLib-0.02 from CPAN.
    gitpan-cpan-distribution: PDFLib
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MSERGEANT/PDFLib-0.02.tar.gz
    gitpan-cpan-author:       MSERGEANT
    gitpan-cpan-maturity:     released

--- PDFLib.pm
+++ PDFLib.pm
@@ -7,7 +7,7 @@ use vars qw/$VERSION/;
 
 use pdflib_pl 4.0;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 my %stacklevel = (
         object => 0,
@@ -31,7 +31,7 @@ sub new {
     $pdf->filename($pdf->{filename} || "");
     for my $info (qw(creator author title)) {
         if (exists $params{$info}) {
-            $pdf->info($info, $params{$info});
+            $pdf->info(ucfirst($info), $params{$info});
         }
     }
     
@@ -69,7 +69,7 @@ sub finish {
 
 sub delete {
     my $pdf = shift;
-    warn("PDF_delete\n");
+#    warn("PDF_delete\n");
     PDF_delete($pdf->{pdf});
 }
 
@@ -173,27 +173,24 @@ sub set_font {
     my $pdf = shift;
     my %params = @_; # expecting: face, size, bold, italic
     
-    if (_equals_font($pdf->{current_font}, %params)) {
-        # warn("Only setting size of $pdf->{current_font}{face} to $params{size}\n");
-        # warn("PDF_setfont(\$p, $pdf->{current_font}->{handle}, $params{size});\n");
-        return PDF_setfont($pdf->_pdf, $pdf->{current_font}->{handle}, $params{size});
+    if (exists $pdf->{current_font} &&
+                $pdf->{current_font}->{face} eq $params{face}) 
+    {
+        return PDF_setfont($pdf->_pdf, $pdf->{current_font}->{handle}, $params{size} || 12.0);
     }
     
     my $fontstring = ucfirst($params{face});
-    $fontstring .= '-' if $params{bold} || $params{italic};
-    $fontstring .= 'Bold' if $params{bold};
-    $fontstring .= 'Italic' if $params{italic};
-    
-    my %font = %params;
-    delete $font{size};
-    
-    $pdf->{current_font} = \%font;
     
     # warn("PDF_findfont(\$p, '$fontstring', 'host', 0);\n");
-    my $font = PDF_findfont($pdf->_pdf, $fontstring, $params{encoding} || 'host', $params{embed} || 0);
+    my $font = PDF_findfont($pdf->_pdf, 
+                ucfirst($params{face}), 
+                $params{encoding} || 'host', 
+                $params{embed} || 0
+                );
     # warn("font: $font\n");
     
     $pdf->{current_font}->{handle} = $font;
+    $pdf->{current_font}->{face} = $params{face};
     
     # warn("font handle: $font (size: $params{size})\n");
     
@@ -417,6 +414,16 @@ sub img {
     return $self->{handle};
 }
 
+sub width {
+    my $self = shift;
+    return $self->{pdf}->get_value("imagewidth", $self->img);
+}
+
+sub height {
+    my $self = shift;
+    return $self->{pdf}->get_value("imageheight", $self->img);
+}
+
 sub close {
     my $self = shift;
     PDF_close_image(shift, $self->{handle});
@@ -437,6 +444,406 @@ PDFLib - More OO interface to pdflib_pl.pm
 =head1 DESCRIPTION
 
 A cleaner API than pdflib_pl.pm, which is a very low-level (non-OO) interface.
+
+=head1 PDFLib API
+
+=head2 new(...)
+
+Construct a new PDF object. No parameters required.
+
+Parameters are passed as name/value pairs (i.e. a hash):
+
+=over 4
+
+=item filename
+
+A filename to save the PDF to. If not supplied the PDF will be generated in
+memory.
+
+=item papersize
+
+The papersize can either be an array ref of [x, y], or can be a string 
+containing one of the below listed paper sizes. This defaults to "a4".
+
+=item creator
+
+The creator of the document.
+
+=item author
+
+The author of the document
+
+=item title
+
+The title of the document
+
+=item orientation
+
+The orientation of the pages. This defaults to "portrait".
+
+=back
+
+Example:
+
+  my $pdf = PDFLib->new(creator => "My PDF Program", 
+        author => "Me",
+        title => "Business Report");
+
+=head2 finish
+
+Let PDFLib know you are finished processing this PDF. This method should
+not normally need to be called, as it is called automatically for you.
+
+=head2 delete
+
+Only call this if you are manually calling finish() also. It deletes the
+used memory for this PDF.
+
+=head2 get_buffer
+
+If (and only if) you didn't supply a filename in the call to new(), then
+get_buffer will return to you the PDF as a string. Very useful for generating
+PDFs on the fly for a web server.
+
+=head2 filename(...)
+
+A getter and setter method for the PDF's filename. Pass in a filename as
+a string to set a new filename. returns the old filename.
+
+=head2 info(key => value)
+
+A getter and setter method for the PDF info fields (such as Title, Creator,
+Author, etc). A key is required. If you pass in a value it will set the
+new value. Returns the old value.
+
+=head2 papersize(...)
+
+A getter and setter for the current paper size. An optional value that can
+be an array ref of [x, y], or a string from the list of paper sizes below,
+will set the current paper size. Returns the old/current paper size.
+
+=head2 orientation(...)
+
+A getter and setter for the current page orientation. All this really does
+is swap the x and y values in the paper size if orientation == "landscape".
+Returns the current/old orientation.
+
+=head2 start_page(...)
+
+Start a new page. If a page has already been started, this will call end_page()
+automatically for you.
+
+Options are passed in as name/value pairs, and are passed to PDFLib::Page->new()
+below.
+
+=head2 end_page
+
+End the current page. It should not normally be necessary to call this.
+
+=head2 set_font(...)
+
+Set the current font being used. The parameters allowed are:
+
+=over 4
+
+=item face
+
+The font face to use. Best to choose from one of the 14 builtin fonts:
+
+  Courier
+  Courier-Bold
+  Courier-BoldOblique
+  Courier-Oblique
+  Helvetica
+  Helvetica-Bold
+  Helvetica-BoldOblique
+  Helvetica-Oblique
+  Symbol
+  Times-Roman
+  Times-Bold
+  Times-BoldItalic
+  Times-Italic
+  ZapfDingbats
+
+=item size
+
+The font size in points. This defaults to 12.0
+
+=item encoding
+
+One of "host" (default), "builtin", "winansi", "ebcdic", or "macroman".
+
+See the pdflib documentation for more details.
+
+=item embed
+
+If set to a true value, this will embed the font in the PDF file. This can
+be useful if using fonts outside of the 14 listed above, but extra font
+metrics information is required and you will need to read the pdflib
+documentation for more information.
+
+=back
+
+=head2 set_text_pos(x, y)
+
+Sets the current text output position.
+
+=head2 get_text_pos
+
+Returns the current text output position as a list (x, y).
+
+=head2 print($text)
+
+Prints the text passed as a parameter to the current page (and creates
+a new page if there is no current page) at the current output position.
+
+Note: this will B<not> wrap, and text can and will fall off the edge of
+your page.
+
+=head2 print_at($text, x => $x, y => $y)
+
+Prints text at the given X and Y coordinates.
+
+=head2 print_boxed($text, ...)
+
+This is perhaps the most interesting output method as it allows you to define
+a bounding box to put the text into, and PDFLib will wrap the text for you.
+
+The parameters you can pass are:
+
+=over 4
+
+=item mode
+
+One of "left", "right", "center", "justify" or "fulljustify".
+
+=item blind
+
+This parameter allows you to output invisible text. Useful for testing
+whether the text will fit into your bounding box.
+
+=item x and y
+
+The X and Y positions (bottom left hand corner) of your bounding box.
+
+=item w and h
+
+The width and height of your bounding box.
+
+=back
+
+Returns zero, or the number of characters from your text that would not
+fit into the box.
+
+=head2 print_line($text)
+
+Print the text at the current output position, with a carriage return
+at the end.
+
+=head2 get_value($key, [$modifier])
+
+There are many values that you can retrieve from pdflib, all are covered
+in the extensive documentation. This method is a wrapper for that.
+
+=head2 set_value($key => $value)
+
+PDFLib also allows you to set values. This method does just that. Note that
+not all values that you can "get" allow you to also "set" that value. Read
+the pdflib documentation for more information on values you can set.
+
+=head2 get_parameter($param, [$modifier])
+
+This is very similar to get_value above. No, I don't know why pdflib makes
+this distinction, before you ask :-)
+
+=head2 set_parameter($param => $value)
+
+Same again. See the pdflib docs for which options are available.
+
+=head2 load_image(...)
+
+Load an image. Parameters available are:
+
+=over 4
+
+=item filetype
+
+One of "png", "gif", "jpeg", or "tiff". Unfortunately PDFLib does not do
+filetype sniffing, yet.
+
+=item filename
+
+The name of the image file to open.
+
+=item stringparam and intparam
+
+See the pdflib documentation for PDF_open_image for more details.
+
+=back
+
+This returns a PDFLib::Image object.
+
+=head2 add_image(...)
+
+Add an image to the current page (or creates a new page if necessary).
+
+Options are passed as name/value pairs. Available options are:
+
+=over 4
+
+=item img
+
+The PDFLib::Image object, returned from load_image() above.
+
+=item x
+
+The x coordinate
+
+=item y
+
+The y coordinate
+
+=item scale
+
+The scaling of the image. Note that only full scaling is possible, not
+separate X and Y scaling. This defaults to 1.0.
+
+=back
+
+=head2 add_bookmark(...)
+
+Adds a bookmark to the PDF file (normally displayed in a tree view on the left
+hand side of the pages in Adobe acrobat reader). Takes the following parameters:
+
+=over 4
+
+=item text
+
+The text of the bookmark
+
+=item parent_of
+
+The parent bookmark for generating hierarchies. This should be a value returned
+from a previous call to add_bookmark, e.g.
+
+  my $root_bm = $pdf->add_bookmark(text => "My Root Bookmark");
+  $pdf->add_bookmark(text => "Child Bookmark", parent_of => $root_bm);
+
+=item open
+
+Whether this bookmark is expanded by default when the PDF is first opened.
+
+=back
+
+=head2 add_link(...)
+
+Turns a square area of the page into a web link. Takes the following parameters:
+
+=over 4
+
+=item x, y, w, h
+
+X and Y coordinates of the lower left hand side of the box, and width and
+height of the box.
+
+=item link
+
+The actual link. Must start with one of "http:", "https:", "ftp:", or
+"mailto:".
+
+=back
+
+=head2 set_border_style($style, $width)
+
+The border in question here is a border around a link. Style must be one
+of "solid" or "dashed". Note that links have a border around them by default,
+so you need to unset that with:
+
+  $pdf->set_border_style("solid", 0);
+
+Unless you want all your links to have ugly boxes around them.
+
+=head1 PDFLib::Image API
+
+The following methods are available on the object returned from load_image
+above.
+
+=head2 width
+
+Return the image's width in points.
+
+=head2 height
+
+Return the image's height in points.
+
+=head1 Default Paper Sizes
+
+The following paper sizes are available. Units are in "points". Any of these
+can be rotated by providing an orientation of "landscape". Alternate paper sizes
+can be used by passing an array ref of [x, y] to anything requiring a
+papersize, but that generally shouldn't be necessary.
+
+=over 4
+
+=item a0
+
+2380 x 3368
+
+=item a1
+
+1684 x 2380
+
+=item a2
+
+1190 x 1684
+
+=item a3
+
+842 x 1190
+
+=item a4
+
+595 x 842
+
+=item a5
+
+421 x 595
+
+=item a6
+
+297 x 421
+
+=item b5
+
+501 x 709
+
+=item letter
+
+612 x 792
+
+=item legal
+
+612 x 1008
+
+=item ledger
+
+1224 x 792
+
+=item 11x17
+
+792 x 1224
+
+=item slides
+
+612 x 450
+
+=back
+
+=head1 TODO
+
+Lots more of the pdflib API needs to be added and tested here. Notably the
+support for other types of attachments, and support for all of the
+graphics primatives.
 
 =head1 AUTHOR
 
